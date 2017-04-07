684da65d1521ce30d3ef594533987579727dd59e
Laura Tsai <103306021@nccu.edu.tw>
2017-04-07 12:00:23 +0800

    把addWorkList存同一個form
    修改models/worklist
    routers/addworlist
    views/addworklist

--- routes/WorkList/addWorkList.js
+++ routes/WorkList/addWorkList.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var addWorklist = require('../../models/Worklist');
 var Question = require('../../models/Worklist');
-
+var Info = require('../../models/Worklist');
 
 
 router.get('/', function(req, res) {
@@ -17,31 +17,41 @@ router.get('/', function(req, res) {
     });
 
 
-            res.render('WorkList/addWorkList', {
-              
-                member: req.session.member || null
-            });
+    res.render('WorkList/addWorkList', {
+
+        member: req.session.member || null
+    });
 
 });
 
+//抓到top table
 // router.post('/', function(req, res, next) {
-//     var newaddWorklist = new Product();
-//     //  newProduct.Q1List(function(err,Q1List){
-//     //    if(err){
-//     //      next(err);
-//     //    }else{
-//     //      console.log('Q1List'+Q1List);
-//     //      console.log('Q1List'+Q1List[0].Q1_Mat);
-//     res.render('WorkList/addWorkList', {
-//         //  Q1List : Q1List,
-//         member: req.session.member || null
+//     console.log('req.body.CarId ' + req.body.CarId);
+//     var newInfo = new Info({
+//         CarId: req.body.CarId,
+//         Miles: req.body.Miles,
+//         InDate: req.body.InDate,
+//         ExpectDate: req.body.ExpectDate
+//     });
+
+//     newInfo.InfoList(function(err, InfoList) {
+//         console.log(req.body.theForm);
+//         res.render('WorkList/addWorkList', {
+//             //  Q1List : Q1List,
+//             member: req.session.member || null
+//         });
 //     });
 // });
 
 //抓到part
 router.post('/', function(req, res, next) {
     console.log('req.body.part' + req.body.Part);
+    console.log('req.body.CarId' + req.body.CarId);
     var newQuestion = new Question({
+        CarId: req.body.CarId,
+        Miles: req.body.Miles,
+        InDate: req.body.InDate,
+        ExpectDate: req.body.ExpectDate,
         Part: req.body.Part,
         Wrong: req.body.Wrong
     });
