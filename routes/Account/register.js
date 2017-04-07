46dd846027d8e034c7a498e1853f85312d233f53
103306068 <hsy.cynthia@Cynthias-MacBook-Pro.local>
2017-04-07 13:35:18 +0800

    header樣式修改 登出js更改 維修項目button-group

--- routes/Account/register.js
+++ routes/Account/register.js
@@ -5,34 +5,34 @@ var Member = require('../../models/Member');
 var async = require('async');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Account/register', {
-    member : null
-  });
+    res.render('Account/register', {
+        member: null
+    });
 });
 router.post('/', function(req, res, next) {
 
-  //首先必須先產生出一個Member的物件在進行save
-  var newMember = new Member({
-    name : req.body.name,
-    account : req.body.account,
-    password : req.body.password
-  });
-  newMember.save(function(err) {
-    if(err) {
-      next(err);
-    } else {
-      //再重新導向之前，我們要讓使用者登入，因此我們需要使用到session
-      req.session.member = newMember;
-      res.redirect('/');
+    //首先必須先產生出一個Member的物件在進行save
+    var newMember = new Member({
+        name: req.body.name,
+        account: req.body.account,
+        password: req.body.password
+    });
+    newMember.save(function(err) {
+        if (err) {
+            next(err);
+        } else {
+            //再重新導向之前，我們要讓使用者登入，因此我們需要使用到session
+            req.session.member = newMember;
+            res.redirect('/');
 
-    }
-  });
+        }
+    });
 });
 
 /* Log  Out*/
-router.post('/logout', function(req, res, next) {
-  req.session.member = null;
-  res.redirect('/');
+router.get('/logout', function(req, res, next) {
+    req.session.member = null;
+    res.redirect('/');
 });
 
 
